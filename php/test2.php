ad285214465e5ba9485eca166be8187dff146fc9
wei0728 <lewishoung3@gmail.com>
2024-05-11 23:48:42 +0800

    0.0.0

--- php/test2.php
+++ php/test2.php
@@ -15,18 +15,16 @@ var number2 = Math.round(Math.random() * 6) + 1;
 var randomAnswer = number1 + number2;
 
 $.ajax({
-   url: "test.php",
-   method: "POST",
-   dataType: "json",
-   data: {randomAnswer: randomAnswer},
-   success: function (result) {     
+  url: "test.php",
+  method: "POST",
+  dataType: "json",
+  data: {randomAnswer: randomAnswer},
+  success: function (result) {     
     console.log( "hello world");
       alert("result: " + result);
       $("#random").html(result);
- 
    }
  });
-
 });
 
 </script>
